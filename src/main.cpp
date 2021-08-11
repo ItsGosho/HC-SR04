886d5288269af9f7a3ff716a0d4ab99fa8f06ee9
itsgosho <itsgosho2@gmail.com>
2021-08-11 12:39:36 +0300

    Added the trigger/echo logic with a cool down period

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,31 @@ bool isButtonPressed() {
 
     int isPressed = digitalRead(TEST_BUTTON_PIN);
 
-    if(isPressed == HIGH && !isButtonPressedAlready) {
+    if (isPressed == HIGH && !isButtonPressedAlready) {
         isButtonPressedAlready = true;
         return true;
     }
 
-    if(isPressed == HIGH && isButtonPressedAlready) {
+    if (isPressed == HIGH && isButtonPressedAlready) {
         return false;
     }
 
-    if(isPressed == LOW && isButtonPressedAlready) {
+    if (isPressed == LOW && isButtonPressedAlready) {
         isButtonPressedAlready = false;
         return false;
     }
 
     return false;
+}
+
+unsigned long measureSignalLength(String message, int signalType) {
+
+    unsigned long start = micros();
+    while (digitalRead(SR04_ECHO_PIN) == signalType);
+    unsigned long timeUS = micros() - start;
+
+    Serial.println(message);
+    Serial.println(timeUS);
 }
 
 void setup() {
@@ -39,6 +49,13 @@ void setup() {
 void loop() {
 
     if (isButtonPressed()) {
-        Serial.println("Pressed!");
+        digitalWrite(SR04_TRIGGER_PIN, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(SR04_TRIGGER_PIN, LOW);
+
+        measureSignalLength("8 burst pulses sending time!", LOW);
+        measureSignalLength("Distance time", HIGH);
+        delay(60);
+        Serial.println("Cool down period passed! You can measure again!");
     }
 }
