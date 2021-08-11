8a901d97a95369d77b4597ba4eb6b954236a26b3
itsgosho <itsgosho2@gmail.com>
2021-08-11 12:13:45 +0300

    Added a test button with a one time trigger functionality for testing the waves

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include <Arduino.h>
 
+#define SERIAL_BAUD_RATE 9600
+#define SR04_TRIGGER_PIN 10
+#define SR04_ECHO_PIN 11
+#define TEST_BUTTON_PIN 12
+
+bool isButtonPressedAlready = false;
+
+bool isButtonPressed() {
+
+    int isPressed = digitalRead(TEST_BUTTON_PIN);
+
+    if(isPressed == HIGH && !isButtonPressedAlready) {
+        isButtonPressedAlready = true;
+        return true;
+    }
+
+    if(isPressed == HIGH && isButtonPressedAlready) {
+        return false;
+    }
+
+    if(isPressed == LOW && isButtonPressedAlready) {
+        isButtonPressedAlready = false;
+        return false;
+    }
+
+    return false;
+}
 
 void setup() {
+    Serial.begin(SERIAL_BAUD_RATE);
 
-
+    pinMode(SR04_TRIGGER_PIN, OUTPUT);
+    pinMode(SR04_ECHO_PIN, INPUT);
+    pinMode(TEST_BUTTON_PIN, INPUT);
 }
 
 void loop() {
 
-
+    if (isButtonPressed()) {
+        Serial.println("Pressed!");
+    }
 }
