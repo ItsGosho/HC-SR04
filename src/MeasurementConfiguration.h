6e4696171401a2f4395c7ebbb8c1d96c69c1695b
itsgosho <itsgosho2@gmail.com>
2021-08-12 15:50:35 +0300

    Added the measurement configuration builder class

---
+++ src/MeasurementConfiguration.h
@@ -0,0 +1,124 @@
+#ifndef HC_SR04_MEASUREMENTCONFIGURATION_H
+#define HC_SR04_MEASUREMENTCONFIGURATION_H
+
+#include "Arduino.h"
+
+enum class TemperatureUnit {
+    CELSIUS,
+    FAHRENHEIT
+};
+
+enum class DistanceUnit {
+    CENTIMETERS,
+    METERS
+};
+
+class MeasurementConfiguration {
+
+private:
+
+    unsigned int* timeoutMS;
+    unsigned int* samples;
+    unsigned int* distance;
+    DistanceUnit* distanceUnit;
+    float* temperature;
+    TemperatureUnit* temperatureUnit;
+    float* humidityPercentage;
+
+public:
+    class builder;
+
+    MeasurementConfiguration(unsigned int* timeoutMs, unsigned int* samples, unsigned int* distance, float* temperature, float* humidityPercentage)
+            : timeoutMS(timeoutMs), samples(samples), distance(distance), temperature(temperature), humidityPercentage(humidityPercentage) {
+    }
+
+    unsigned int* getTimeoutMS() const {
+        return this->timeoutMS;
+    }
+
+    unsigned int* getSamples() const {
+        return this->samples;
+    }
+
+    unsigned int* getDistance() const {
+        return this->distance;
+    }
+
+    DistanceUnit* getDistanceUnit() const {
+        return this->distanceUnit;
+    }
+
+    float* getTemperature() const {
+        return this->temperature;
+    }
+
+    TemperatureUnit* getTemperatureUnit() const {
+        return this->temperatureUnit;
+    }
+
+    float* getHumidityPercentage() const {
+        return this->humidityPercentage;
+    }
+};
+
+class MeasurementConfiguration::builder {
+
+private:
+    unsigned int* timeoutMS;
+    unsigned int* samples;
+    unsigned int* distance;
+    DistanceUnit* distanceUnit;
+    float* temperature;
+    TemperatureUnit* temperatureUnit;
+    float* humidityPercentage;
+
+public:
+    builder() {
+        this->timeoutMS = nullptr;
+        this->samples = nullptr;
+        this->distance = nullptr;
+        this->distanceUnit = nullptr;
+        this->temperature = nullptr;
+        this->temperatureUnit = nullptr;
+        this->humidityPercentage = nullptr;
+    }
+
+    builder& withTimeoutMS(const unsigned int& timeoutMS) {
+        this->timeoutMS = &const_cast<unsigned int&>(timeoutMS);
+        return *this;
+    }
+
+    builder& withSamples(const unsigned int& samples) {
+        this->samples = &const_cast<unsigned int&>(samples);
+        return *this;
+    }
+
+    builder& withDistance(const unsigned int& distance, const DistanceUnit& distanceUnit) {
+        this->distance = &const_cast<unsigned int&>(distance);
+        this->distanceUnit = &const_cast<DistanceUnit&>(distanceUnit);
+        return *this;
+    }
+
+    builder& withTemperature(const float& temperature, const TemperatureUnit& temperatureUnit) {
+        this->temperature = &const_cast<float&>(temperature);
+        this->temperatureUnit = &const_cast<TemperatureUnit&>(temperatureUnit);
+        return *this;
+    }
+
+    builder& withHumidity(const float& humidityPercentage) {
+        this->humidityPercentage = &const_cast<float&>(humidityPercentage);
+        return *this;
+    }
+
+    MeasurementConfiguration build() const {
+
+        return MeasurementConfiguration(this->timeoutMS,
+                                        this->samples,
+                                        this->distance,
+                                        this->temperature,
+                                        this->humidityPercentage);
+    }
+
+};
+
+#endif //HC_SR04_MEASUREMENTCONFIGURATION_H
